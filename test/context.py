58b88d43d4ae60b3ff9d0e6952fbd3fac5200487
natez56 <natez56@hotmail.com>
2018-07-18 15:09:05 -0700

    Adding unit tests

---
+++ test/context.py
@@ -0,0 +1,10 @@
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../nightfall')))
+
+import nightfall
+import file_manager
+import game_classes
+import game_methods
+import text_parser
